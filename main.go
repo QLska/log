ca39592b3efd97977214a23888c3e369b0f2339b
BXL <bcoder@gmail.com>
2023-08-22 11:22:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"syscall"
 	"time"
 )
 
@@ -414,17 +413,5 @@ func createLevelLogger(level Level, t time.Time) *log.Logger {
 }
 
 func dealFatal() {
-	if !Exist(path) {
-		err := os.Mkdir(path, 0666)
-		if err != nil {
-			fmt.Println("deal fatal failed! ", err)
-			os.Exit(1)
-		}
-	}
-	logFile, err := os.OpenFile(path+"/fatal.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-	if err != nil {
-		fmt.Println("deal fatal failed! ", err)
-		os.Exit(1)
-	}
-	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
+
 }
